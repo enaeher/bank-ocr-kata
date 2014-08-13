c85f8f4fc106daded1d0927e9e735888d74ce27e
Eli Naeher <enaeher@gmail.com>
2014-08-13 18:20:07 -0500

    Additional utility functions for converting integers to lists of digits and vice versa.

--- util.lisp
+++ util.lisp
@@ -11,3 +11,19 @@
 
 (defun flatten-once (list)
   (apply #'append list))
+
+(defun digit-list->integer (digit-list)
+  (apply '+
+         (loop
+            :with digit-list := (reverse digit-list)
+            :for digit :in digit-list
+            :for exponent :from 0 :below (length digit-list)
+            :collecting (* digit (expt 10 exponent)))))
+
+(defun integer->digit-list (integer)
+  (loop
+     :for exponent :from (floor (log integer 10)) :downto 0
+     :collecting (multiple-value-bind (digit new-integer)
+                     (floor integer (expt 10 exponent))
+                   (setf integer new-integer)
+                   digit)))
